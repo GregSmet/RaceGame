90abef65df77d817b6b136a5939cc8ca6cc69102
GregSmet <lolkeklmaoxdswaggster@gmail.com>
2023-11-20 22:46:52 +0300

    changing directory

---
+++ data/button.py
@@ -0,0 +1,23 @@
+from pygame.sprite import Sprite
+
+class Button(Sprite):
+    def __init__(self, group, main, image):
+        super().__init__(group)
+        main.add(self)
+        self.image = image
+        self.rect = self.image.get_rect()
+
+
+    def press(self):
+        pass
+
+
+
+    def remove(self):
+        self.rect.x = 2000
+        self.rect.y = 2000
+
+
+
+    def im_change(self, im):
+        self.image = im
